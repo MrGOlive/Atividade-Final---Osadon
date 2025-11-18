65a15a704db6422ec7ea3a156776cf3167182ef8
matheus.santos <math.filho1996@gmail.com>
2025-11-18 10:37:19 -0300

    iniciando configurações do knex

---
+++ knexfile.js
@@ -0,0 +1,24 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: 'mysql2',
+    connection: {
+      host: process.env.DB_HOST || '127.0.0.1',
+      port: process.env.DB_PORT || 3306,
+      user: process.env.DB_USER || 'root',
+      password: process.env.DB_PASSWORD || '',
+      database: process.env.DB_NAME || 'db_biblioteca'
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './migrations' // Onde os arquivos de migração serão armazenados
+    },
+    seeds: {
+      directory: './seeds'
+    }
+  }
+};
