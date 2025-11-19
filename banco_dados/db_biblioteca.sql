b6e884018444d6d26deaab6cfe8d668cf3d915db
marina <marinamagalhaesc@gmail.com>
2025-11-19 15:33:20 -0300

    db_biblioteca excluida creação de tabela

--- banco_dados/db_biblioteca.sql
+++ banco_dados/db_biblioteca.sql
@@ -2,13 +2,4 @@ CREATE SCHEMA db_biblioteca;
 
 USE db_biblioteca;
 
-CREATE TABLE IF NOT EXISTS usuarios (
-	ID int auto_increment primary key,
-    NOME varchar(255) not null,
-    CPF varchar(11) not null unique,
-    EMAIL varchar(255) not null unique,
-    TELEFONE varchar(11) not null,
-    DATA_NASCIMENTO date not null
-);
-
 
