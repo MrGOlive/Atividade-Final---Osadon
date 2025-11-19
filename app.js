a0f8d8c747411a48e786af417559c4a988f6624f
josematheus7 <math.filho1996@gmail.com>
2025-11-19 15:47:12 -0300

    read parcial

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express") // Chamando a classe "express"
 const path = require('path');
+const knex = require("./banco_dados/db")
 
 const app = express() // Instanciando a classe "express" como o objeto "app" 
 
 app.get("/home", (req, res)=>{
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    knex('usuarios').then(data => {
+        console.log(data)
+    });
 })
 
 app.get("/cadastro", (req, res)=>{
